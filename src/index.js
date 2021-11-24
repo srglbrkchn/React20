45c2420bc113516cd2ab45d30d042aa11718e025
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-24 17:01:14 +0100

    Turn anonymous function inside find function into an inline fat arrow function.

--- src/index.js
+++ src/index.js
@@ -17,16 +17,14 @@ var numbers = [3, 56, 2, 48, 5];
 // const newNumbers = numbers.filter(num => num < 10);
 // console.log(newNumbers);
 
-// Reduce - Accumulate a value by doing something to each item in an array.
-var newNumber = numbers.reduce( (accumulator, currentNumber) => accumulator + currentNumber); 
+// // Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce( (accumulator, currentNumber) => accumulator + currentNumber); 
+// console.log(newNumber);
 
+
+//Find - find the first item that matches from an array.
+const newNumber = numbers.find(num =>  num > 10);
 console.log(newNumber);
-
-
-////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
 
 ////FindIndex - find the index of the first item that matches.
 // const newNumber = numbers.findIndex(function (num) {
