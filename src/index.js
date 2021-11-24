a1d87a16225a183dcf3320e7a10738436ba2328b
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-24 16:57:26 +0100

    Turn anonymous function inside filter function into an inline fat arrow function.

--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ var numbers = [3, 56, 2, 48, 5];
 // console.log(newNumbers);
 
 //Map -Create a new array by doing something with each item in an array.
-const newNumbers = numbers.map( x => x * 2);
-//console.log(newNumbers);
+// const newNumbers = numbers.map( x => x * 2);
+// console.log(newNumbers);
 
-//////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+////Filter - Create a new array by keeping the items that return true.
+const newNumbers = numbers.filter(num => num < 10);
+console.log(newNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 // var newNumber = numbers.reduce(function (accumulator, currentNumber) {
